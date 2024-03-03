a6fc799ddf2de7a7fbd0a6b4ea89cc3ba5eea984
abhishek-sharma-2001 <abhishek.sharma.12.10.2001@gmail.com>
2024-03-03 17:45:14 +0530

    added hamberger menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,34 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import "../App.css";
+import { Container, Navbar, Nav } from "react-bootstrap";
 import logo from "../assets/MPCorner_logo.png";
-import { Container } from "react-bootstrap";
 
 const Header = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <Container>
-      <div className="header">
+      <Navbar expand="lg" className="header">
         <div className="left-content">
           <img src={logo} alt="MP Corner Logo" className="logo" />
           <div className="left-title">MP Corner</div>
         </div>
-        <div className="right-links">
-          <Link to="/">Home</Link>
-          {/* <Link to="/about">About</Link> */}
-          <Link to="/performance">Performance</Link>
-          <Link to="/activity">Activity</Link>
-        </div>
-      </div>{" "}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={toggleMenu} />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <div className="right-links">
+              <Link to="/">Home</Link>
+              {/* <Link to="/about">About</Link> */}
+              <Link to="/performance">Performance</Link>
+              <Link to="/activity">Activity</Link>
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </Container>
   );
 };
