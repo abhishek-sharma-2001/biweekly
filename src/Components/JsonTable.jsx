cedabbc0a00eb976afa9ac94b33d4c4b4264cda1
abhishek-sharma-2001 <abhishek.sharma.12.10.2001@gmail.com>
2024-03-10 12:35:13 +0530

    finalized the table using json api

--- src/Components/JsonTable.jsx
+++ src/Components/JsonTable.jsx
@@ -1,28 +1,54 @@
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Table } from 'react-bootstrap';
 
-const JsonTable = ({ jsonData }) => {
-  const headers = Object.keys(jsonData[0]);
+const JsonTable = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://api.jsonbin.io/v3/b/65ed574c266cfc3fde964421');
+        setData(response.data.record);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          {headers.map((header) => (
-            <th key={header}>{header}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {jsonData.map((item, index) => (
-          <tr key={index}>
-            {headers.map((header) => (
-              <td key={header}>{item[header]}</td>
-            ))}
+    <div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Domain Name</th>
+            <th>Issue Count</th>
+            <th>Issue Score</th>
+            <th>Blocker</th>
+            <th>Critical</th>
+            <th>Major</th>
+            <th>Normal</th>
+            <th>Minor</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {data.map(item => (
+            <tr key={item["Domain Name"]}>
+              <td>{item["Domain Name"]}</td>
+              <td>{item["Issue Count"]}</td>
+              <td>{item["Issue Score"]}</td>
+              <td>{item["Blocker"] || '0'}</td>
+              <td>{item["Critical"] || '0'}</td>
+              <td>{item["Major"] || '0'}</td>
+              <td>{item["Normal"] || '0'}</td>
+              <td>{item["Minor"] || '0'}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 };
 
